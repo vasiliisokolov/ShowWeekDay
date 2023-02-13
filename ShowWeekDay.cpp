c2fadd378163fd7463e1081980989acb3671dcfd
VasiliiSokolov <vasilysokolov@mail.ru>
2023-02-13 19:40:56 +0300

    Добавьте файлы проекта.

---
+++ ShowWeekDay.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+#define DAY_0 "Monday"
+#define DAY_1 "Tuesday"
+#define DAY_2 "Wednesday"
+#define DAY_3 "Thursday"
+#define DAY_4 "Friday"
+#define DAY_5 "Saturday"
+#define DAY_6 "Sunday"
+#define CURRENT_DAY(count) DAY_ ## count
+
+
+
+int main() {
+	int data;
+	std::cout << "Enter number of a day: ";
+	std::cin >> data;
+
+	std::cout << CURRENT_DAY(day);
+
+}
